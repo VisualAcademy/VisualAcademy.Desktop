db56b95d2d43ccd6149fe769cbb5313351f01431
Park YongJune <redplus@redplus.net>
2023-02-02 13:02:55 +0900

    TodoManager 폼 생성

---
+++ src/VisualAcademy.Desktop/TodoApp/TodoManager.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace VisualAcademy.Desktop.TodoApp
+{
+    /// <summary>
+    /// TodoManager.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class TodoManager : Window
+    {
+        public TodoManager()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("WPF");
+        }
+
+        private void btnAddTodo_Click(object sender, RoutedEventArgs e)
+        {
+            btnAddTodo.Content = "Add";
+        }
+    }
+}
